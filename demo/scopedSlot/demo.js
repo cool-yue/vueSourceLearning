304cc40da72582f926f96703d1ac752ae408c26a
yue <379175365@qq.com>
2018-12-25 23:56:52 +0800

    vuelearning 20181225night

--- demo/scopedSlot/demo.js
+++ demo/scopedSlot/demo.js
@@ -1,58 +1,51 @@
-Vue.component("myDiv",{
-	template:"<div><slot>我是默认的</slot></div>",
-	mounted() {
-		console.log("myDiv this.$slots",this.$slots);
-		console.log("myDiv",this.$options);
-	}
-});
+// Vue.component("myDiv",{
+// 	template:"<div><slot>我是默认的</slot></div>",
+// 	mounted() {
+// 		console.log("myDiv this.$slots",this.$slots);
+// 		console.log("myDiv",this.$options);
+// 	}
+// });
 
+// Vue.component("child",{
+// 	template:`<div class="child">
+//                <h3>这里是子组件</h3>
+//                <slot :data="data"></slot>
+//              </div>`,
+//     data() {
+// 		return {
+// 	       data: ['zhangsan','lisi','wanwu','zhaoliu','tianqi','xiaoba'],
+// 		}
+// 	},
+// 	beforeCreate() {
+// 		console.log(this.data);
+// 	},
+// 	mounted() {
+// 		console.log(this.data);
+// 	}
+// });
 
-
-
-;
-
-
-
-Vue.component("child",{
-	template:`<div class="child">
-               <h3>这里是子组件</h3>
-               <slot :data="data"></slot>
-             </div>`,
-    data() {
-		return {
-	       data: ['zhangsan','lisi','wanwu','zhaoliu','tianqi','xiaoba'],
-		}
-	},
-	beforeCreate() {
-		console.log(this.data);
-	},
-	mounted() {
-		console.log(this.data);
-	}
-});
-
-Vue.component("myLi",{
-	template:`<li class="dx-li">	
-                <slot str="你好 掘金！">
-	                hello juejin!
-                </slot>
-            </li>`,
-    data() {
-		return {
-	        todos:[
-			    {id:1,a:true,text:"one"},
-				{id:2,a:false,text:"one"},
-			    {id:3,a:true,text:"one"},
-			]
-		}
-	},
-	beforeCreate() {
-		console.log(this.todos);
-	},
-	mounted() {
-		console.log(this.todos);
-	}
-})
+// Vue.component("myLi",{
+// 	template:`<li class="dx-li">	
+//                 <slot str="你好 掘金！">
+// 	                hello juejin!
+//                 </slot>
+//             </li>`,
+//     data() {
+// 		return {
+// 	        todos:[
+// 			    {id:1,a:true,text:"one"},
+// 				{id:2,a:false,text:"one"},
+// 			    {id:3,a:true,text:"one"},
+// 			]
+// 		}
+// 	},
+// 	beforeCreate() {
+// 		console.log(this.todos);
+// 	},
+// 	mounted() {
+// 		console.log(this.todos);
+// 	}
+// })
 
 /*
 const app = new Vue({
@@ -72,7 +65,7 @@ const app = new Vue({
 */
 
 Vue.component("abc",{
-	tempalte:"<div>我是{{mes}}</div>",
+	template:"<div>我是{{mes}}</div>",
 	data() {
 		return {
 			mes:"abc"
@@ -89,9 +82,5 @@ app1 = new Vue({
 		}
 	}
 });
-
-
-
-
 
 app1.$mount(document.getElementById("app"));
